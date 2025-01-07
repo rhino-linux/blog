e7a1109ffa08e8800fc8da1fc295526898adf977
ook37 <oren+12345@taumoda.com>
2025-01-07 16:02:13 -0500

    add action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.jsx'
-})
- 
-module.exports = withNextra()
+  themeConfig: './theme.config.jsx',
+});
+
+module.exports = withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+});
